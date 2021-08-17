29b1ab9a9cf79e6640ab0ed3ae5bfa9398c03d98
fertilewaif <gmkornilov@edu.hse.ru>
2021-08-17 19:19:17 +0300

    add basics for search screen

--- app/src/main/java/com/gmkornilov/shikimori/presentation/MainActivity.kt
+++ app/src/main/java/com/gmkornilov/shikimori/presentation/MainActivity.kt
@@ -2,9 +2,12 @@ package com.gmkornilov.shikimori.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.widget.SearchView
 import com.github.terrakok.cicerone.BackTo
 import com.github.terrakok.cicerone.NavigatorHolder
 import com.github.terrakok.cicerone.Replace
+import com.github.terrakok.cicerone.Router
 import com.github.terrakok.cicerone.androidx.AppNavigator
 import com.gmkornilov.shikimori.R
 import com.gmkornilov.shikimori.presentation.navigation.Screens
@@ -15,6 +18,9 @@ import javax.inject.Inject
 class MainActivity : AppCompatActivity() {
     @Inject
     lateinit var navigatorHolder: NavigatorHolder
+
+    @Inject
+    lateinit var router: Router
 
     private val navigator = object : AppNavigator(this, R.id.mainContainer) {
         fun hasScreens(): Boolean {
@@ -27,7 +33,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         if (savedInstanceState == null) {
-            navigator.applyCommands(arrayOf(BackTo(null), Replace(Screens.MainScreen())))
+            router.newRootScreen(Screens.MainScreen())
         }
 
         supportActionBar?.apply {
@@ -46,6 +52,17 @@ class MainActivity : AppCompatActivity() {
     override fun onPause() {
         super.onPause()
         navigatorHolder.removeNavigator()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_activity_menu, menu)
+
+        val menuItem = menu.findItem(R.id.search)
+        val searchView = menuItem.actionView as SearchView
+
+        searchView.queryHint = getString(R.string.search_anime)
+
+        return true
     }
 
     override fun onSupportNavigateUp(): Boolean {
