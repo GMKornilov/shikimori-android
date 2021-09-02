aa9ee50e4ca36619d96796805949d4ff2a78248c
fertilewaif <gmkornilov@edu.hse.ru>
2021-09-03 00:36:31 +0300

    add toolbar back buttonm for navigation

--- app/src/main/java/com/gmkornilov/shikimori/presentation/navigation/backstacks/BackstackFragment.kt
+++ app/src/main/java/com/gmkornilov/shikimori/presentation/navigation/backstacks/BackstackFragment.kt
@@ -1,13 +1,18 @@
 package com.gmkornilov.shikimori.presentation.navigation.backstacks
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.vectordrawable.graphics.drawable.AnimationUtilsCompat
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.github.terrakok.cicerone.Navigator
 import com.github.terrakok.cicerone.NavigatorHolder
 import com.github.terrakok.cicerone.Router
 import com.github.terrakok.cicerone.androidx.AppNavigator
 import com.gmkornilov.shikimori.R
+import com.gmkornilov.shikimori.databinding.BackstackLayoutBinding
 import com.gmkornilov.shikimori.presentation.ShikimoriApplication
 import com.gmkornilov.shikimori.presentation.navigation.RouterProvider
 import javax.inject.Inject
@@ -21,6 +26,8 @@ class BackstackFragment : Fragment(R.layout.backstack_layout), BackPressedListen
         AppNavigator(requireActivity(), R.id.backstackContainer, childFragmentManager)
     }
 
+    private val binding: BackstackLayoutBinding by viewBinding(BackstackLayoutBinding::bind)
+
     @Inject
     lateinit var backstackNavigationManager: BackstackNavigationManager
 
@@ -32,10 +39,29 @@ class BackstackFragment : Fragment(R.layout.backstack_layout), BackPressedListen
         backstackNavigationManager.getCurrentBackstackRouter(backstackInfo.getBackstackName())
     }
 
+    private val backstackChangedListener = FragmentManager.OnBackStackChangedListener {
+        if (childFragmentManager.backStackEntryCount > 0) {
+            with (binding) {
+                toolbar.setNavigationIcon(R.drawable.ic_back_button)
+            }
+        } else {
+            with (binding) {
+                toolbar.navigationIcon = null
+            }
+        }
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         ShikimoriApplication.INSTANCE.appComponent.inject(this)
+
+        childFragmentManager.addOnBackStackChangedListener(backstackChangedListener)
+        with (binding) {
+            toolbar.setNavigationOnClickListener {
+                onBackPressed()
+            }
+        }
 
         if (savedInstanceState == null) {
             router.newRootScreen(backstackInfo.getRootScreen())
